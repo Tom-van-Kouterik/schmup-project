f39149330c24520489cdd698193489c0f202528e
ashen one <tomvankouterik2005@gmail.com>
2024-10-01 14:00:00 +0200

    added base movement

--- Assets/project/Scripts/PlayerMovement.cs
+++ Assets/project/Scripts/PlayerMovement.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private float movementSpeed = 5f;
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        
+        float direction = Input.GetAxisRaw("Horizontal");
+        transform.Translate(direction * Time.deltaTime * movementSpeed, 0, 0);
+
+        if (Input.inputString != "") Debug.Log(Input.inputString);
     }
 }
